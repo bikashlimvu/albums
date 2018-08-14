9df22195cca9afb5541f304823ec034d2c289b79
Bikash Limbu <bikash_limvu@yahoo.com>
2018-08-14 13:22:15 +0100

    album detail component

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,14 +1,73 @@
 //Import libraries to make component
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
+import Card from './card';
+import CardSection from './cardSection';
+import Button from './button';
 
 //Make a component
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image, url } = album;
+    const {
+        headerStyle,
+        headerTextStyle,
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        imageStyle
+    } = styles;
+
     return (
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+            <View style={thumbnailContainerStyle}>
+                <Image
+                style={thumbnailStyle}
+                source={{ uri: thumbnail_image }}
+                />
+            </View>
+            <View style={headerStyle}>
+                <Text style={headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                style={imageStyle}
+                source={{ uri: image }}
+                />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
+            </CardSection>
+        </Card>
     );
+};
+
+const styles = {
+    headerStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
 };
 
 //Make components available to the other parts of the app
